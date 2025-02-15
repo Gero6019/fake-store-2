164917593b9cb48a6beb488533291275f8333333
usuario <usuario@email.com>
2025-02-14 21:13:59 -0300

    agregando el componente BuyList

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,46 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { Products } from './components/Products'
+import { BuyList } from './components/BuyList'
 
 function App() {
   const [products, setProducts] = useState([])
   const [shop, setShop] = useState([])
+  const [shopPage, setShopPage] = useState(false)
 
-  const getProducts = async ()=>{
+  const getProducts = async () => {
     let response = await fetch("https://fakestoreapi.com/products")
     let data = await response.json()
     setProducts(data)
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     getProducts()
-  },[])
+  }, [])
 
-  const buyProduct = (idProduct)=>{
-    setShop([...shop,idProduct])
+  const buyProduct = (product) => {
+    setShop([...shop, product])
   }
 
-  if(products.length == 0){
+  if (products.length == 0) {
     return <>
       <h1>Cargando...</h1>
     </>
   }
 
-  return (
-    <>
-      <h1>Fake Store</h1>
-      <Products product={products} buyProduct={buyProduct} />
+  if (shopPage == false) {
+    return (
+      <>
+        <h1>Fake Store</h1>
+        <button disabled={shop.length == 0} onClick={() => { setShopPage(true) }}>Carrito: {shop.length}</button>
+        <Products product={products} buyProduct={buyProduct} />
+      </>
+    )
+  } else {
+    return <>
+      <BuyList shop={shop} setShopPage={setShopPage} setShop={setShop} />
     </>
-  )
+  }
 }
 
 export default App
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,10 +2,10 @@ export const Products = ({ product, buyProduct }) => {
     return <div className="row">
         {product.map((element, index) => {
             return <div className="col-4" key={index}>
-                <h1>{element.title}</h1>
+                <h4>{element.title}</h4>
                 <img src={element.image} height={200} width={300} alt="" />
                 <h3>${element.price}</h3>
-                <button onClick={()=>{buyProduct(element.id)}}>Comprar</button>
+                <button onClick={()=>{buyProduct(element)}}>Comprar</button>
             </div>
         })}
     </div>
